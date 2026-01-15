6f4ea95f1448e53e0935535f092f7cef70e7d2be
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2026-01-16 04:06:44 +0530

    Set up authentication routes with JWT middleware

--- worksana-backend/routes/auth.route.js
+++ worksana-backend/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { signup, login, signOut } from '../controllers/auth.controller.js';
+import { signup, login, signOut, authenticatedUser } from '../controllers/auth.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", login);
 router.post("/signout", signOut);
+router.get("/me", verifyToken, authenticatedUser);
 
 export default router;
