03dc226d86004de7d1535cf83aa51ae4c2934bc9
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2026-01-17 15:47:01 +0530

    Add JWT token verification middleware

---
+++ worksana-backend/utils/verifyUser.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from './error.js';
+
+export const verifyToken = async (req, res, next) => {
+    const token = req.cookies.access_token;
+    console.log(token)
+    if (!token) {
+        return next(errorHandler(401, "Unauthorized"));
+    }
+
+    jwt.verify(token, process.env.JWT_SECRECT, (err, user) => {
+        if (err) {
+            return next(errorHandler(404, "Forbidden"));
+        }
+        req.user = user;
+        next();
+    });
+};
