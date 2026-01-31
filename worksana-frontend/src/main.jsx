4830dd634f70c575da64a4e7df764e41dfe4d333
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2026-01-31 14:03:52 +0530

    Set up app entry with routing and theming

--- worksana-frontend/src/main.jsx
+++ worksana-frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "next-themes";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+);
